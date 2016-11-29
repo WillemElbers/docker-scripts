2fa6838bc3f38826d0df18e36d4de8f621a6a45e
WillemElbers <willem@clarin.eu>
2016-11-29 09:29:46 +0100

    Added commands to clean volumes and exited containers

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-docker rmi $(docker images -q -f dangling=true)
+#Clean exited containers (CAREFULL):
+docker rm -v $(docker ps -a -q -f status=exited)
+
+#Clean images:
+docker rmi $(docker images -f "dangling=true" -q)
+
+#Clean dangling volumes
+docker volume rm $(docker volume ls -qf dangling=true)
